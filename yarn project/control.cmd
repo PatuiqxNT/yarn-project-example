09f2be8d369ecc366e0a808d78c640daa6c14181
PatuiqxNT <85111751+PatuiqxNT@users.noreply.github.com>
2021-12-19 20:24:14 +0200

    Add files via upload

---
+++ yarn project/control.cmd
@@ -0,0 +1,3 @@
+@Echo off
+cd Project Files
+C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat"
---
+++ yarn project/install_yarn.cmd
@@ -0,0 +1,8 @@
+:: Simple script to install yarn!
+:: Note to install yarn apps you must run control.cmd and run the following command:
+:: "yarn"
+:: and
+:: "yarn init"
+@Echo off
+cd project files
+npm install --global yarn
