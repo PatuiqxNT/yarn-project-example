2d0bda3e2f5a67bad6d3695884a4b58cc11817bc
PatuiqxNT <85111751+PatuiqxNT@users.noreply.github.com>
2021-12-19 20:23:54 +0200

    Delete yarn project directory

--- yarn project/control.cmd
+++
@@ -1,3 +0,0 @@
-@Echo off
-cd Project Files
-C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat"
--- yarn project/install_yarn.cmd
+++
@@ -1,8 +0,0 @@
-:: Simple script to install yarn!
-:: Note to install yarn apps you must run control.cmd and run the following command:
-:: "yarn"
-:: and
-:: "yarn init"
-@Echo off
-cd project files
-npm install --global yarn
